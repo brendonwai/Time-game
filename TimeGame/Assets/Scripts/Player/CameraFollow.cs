9b71ecb279d5fafe1735b9c67a2121584c114dbc
achongco <achongco@uci.edu>
2014-11-04 15:35:37 -0800

    Enabled ability to set max horizontal and vertical view of camera.

--- TimeGame/Assets/Scripts/Player/CameraFollow.cs
+++ TimeGame/Assets/Scripts/Player/CameraFollow.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
-	public Transform Player;
-	static float z = -10f;
-	//public float maxHorizUnits;
-	//public float maxVertUnits;
+
+	public Transform Player;		//Player object
+	static float z = -10f; 			//Camera z position
+
+	//Camera constraints
+	public float MaxHorizonView = 0f;		//Max distance in x direction
+	public float MaxVerticalView = 0f;		//Max distance in y direction
+
+
 	// Use this for initialization
 	void Start () {
 		//Screen.showCursor = true;		//not sure if this works
@@ -16,11 +21,12 @@ public class CameraFollow : MonoBehaviour {
 		float x = Player.position.x;
 		float y = Player.position.y;
 
-		//transform.position = new Vector3 (x, y, z);	//OLD set camera position to center on player
-
 		float mouseX = Camera.main.ScreenToWorldPoint (Input.mousePosition).x;
 		float mouseY = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
-		
+
+		maxViewSet(ref mouseX, ref mouseY, x,y);	//Prevents camera from going beyond max constraints
+
+
 		//Set camera pos to midpoint between player and cursor. Has problems if you try to change the 2s
 		transform.position = new Vector3 ((x + mouseX)/2, (y + mouseY)/2, z);
 
@@ -74,5 +80,22 @@ public class CameraFollow : MonoBehaviour {
 		*/
 		//Debug for camera/mouse/player pos
 		//Debug.Log ("Camera" + transform.position + "Mouse: " + mouseX + ", " + mouseY + "Player: " + x + ", " + y);
+	}
+
+	//Sets camera position to be within max range
+	void maxViewSet(ref float cam_x, ref float cam_y, float char_x, float char_y)
+	{
+		if(cam_x > MaxHorizonView+char_x){
+			cam_x = MaxHorizonView + char_x;
+		}
+		if(cam_x < char_x-MaxHorizonView){
+			cam_x = char_x-MaxHorizonView;
+		}
+		if(cam_y > MaxVerticalView+char_y){
+			cam_y = MaxVerticalView + char_y;
+		}
+		if(cam_y < char_y-MaxVerticalView){
+			cam_y = char_y-MaxVerticalView;
+		}
 	}
 }
