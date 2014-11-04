994905c07fbc784620597d8a4b3794560667a97c
Christopher Luong <bumblefo@gmail.com>
2014-11-03 21:26:32 -0800

    Added Camera that changes to the midpoint between player and camera. Also cleaned up background collider

--- TimeGame/Assets/Scripts/Player/CameraFollow.cs
+++ TimeGame/Assets/Scripts/Player/CameraFollow.cs
@@ -2,14 +2,77 @@
 using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
-
+	public Transform Player;
+	static float z = -10f;
+	//public float maxHorizUnits;
+	//public float maxVertUnits;
 	// Use this for initialization
 	void Start () {
-	
+		//Screen.showCursor = true;		//not sure if this works
+		transform.position = new Vector3(Player.position.x, Player.position.y, z);
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		float x = Player.position.x;
+		float y = Player.position.y;
+
+		//transform.position = new Vector3 (x, y, z);	//OLD set camera position to center on player
+
+		float mouseX = Camera.main.ScreenToWorldPoint (Input.mousePosition).x;
+		float mouseY = Camera.main.ScreenToWorldPoint (Input.mousePosition).y;
+		
+		//Set camera pos to midpoint between player and cursor. Has problems if you try to change the 2s
+		transform.position = new Vector3 ((x + mouseX)/2, (y + mouseY)/2, z);
+
+		/*
+		//Tried to make the camera have a max distance from the player but it didn't really work out
+		if ((mouseX - x) >= maxHorizUnits){
+			//
+			if ((mouseY - y) >= maxVertUnits){
+				//
+				transform.position = new Vector3 ((x + maxHorizUnits)/2, (x + maxVertUnits)/2, z);
+			}
+			else if ((mouseY - y) <= -maxVertUnits){
+				//
+				transform.position = new Vector3 ((x + maxHorizUnits)/2, (x - maxVertUnits)/2, z);
+			}
+			else{
+				//
+				transform.position = new Vector3 ((x + maxHorizUnits)/2, (y + mouseY)/2, z);
+			}
+		}
+		else if ((mouseX - x) <= -maxHorizUnits){
+			//
+			if ((mouseY - y) >= maxVertUnits){
+				//
+				transform.position = new Vector3 ((x - maxHorizUnits)/2, (x + maxVertUnits)/2, z);
+			}
+			else if ((mouseY + y) <= -maxVertUnits){
+				//
+				transform.position = new Vector3 ((x - maxHorizUnits)/2, (x - maxVertUnits)/2, z);
+			}
+			else{
+				//
+				transform.position = new Vector3 ((x - maxHorizUnits)/2, (y + mouseY)/2, z);
+			}
+		}
+		else{
+			//
+			if ((mouseY - y) >= maxVertUnits){
+				//
+				transform.position = new Vector3 ((x + mouseX)/2, (x + maxVertUnits)/2, z);
+			}
+			else if ((mouseY - y) <= -maxVertUnits){
+				//
+				transform.position = new Vector3 ((x + mouseX)/2, (x - maxVertUnits)/2, z);
+			}
+			else{
+				//Sets camera pos to midpoint between player and cursor. Has problems if you try to change the 2s
+				transform.position = new Vector3 ((x + mouseX)/2, (y + mouseY)/2, z);
+			}
+		}
+		*/
+		//Debug for camera/mouse/player pos
+		//Debug.Log ("Camera" + transform.position + "Mouse: " + mouseX + ", " + mouseY + "Player: " + x + ", " + y);
 	}
 }
