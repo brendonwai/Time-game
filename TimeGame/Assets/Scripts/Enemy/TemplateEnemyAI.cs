914fd945b4eb8d46ecb1d8f195b205c613b66ae9
achongco <achongco@uci.edu>
2014-11-07 10:31:12 -0800

    Made the Enemy AI a little more efficient. Just found out it's better to use the in-built physic engine/Collider triggers than constantly calculating the distance between the enemy and the player for detection. But I haven't figured out a way to make the attackRadius depend on another Collider trigger since Unity doesn't allow you to manage two Trigger Colliders on the same GameObject separately.

--- TimeGame/Assets/Scripts/Enemy/TemplateEnemyAI.cs
+++ TimeGame/Assets/Scripts/Enemy/TemplateEnemyAI.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class TemplateEnemyAI : MonoBehaviour {
 	public float moveSpeed = 0.1f;	 	//Sets momvement speed
-	public float detectRadius = 1f;		//Sets radius of enemy detection
 	public float attackRadius = 0.5f;	//Sets when enemy starts attacking
 	public float closeEnough = 0.9f;	//Determines when enemy is close enough to
 											//to player to stop moving towards player
+	CircleCollider2D detectRadius;		//Sets when enemy detects player
 	bool facingRight = true;			//Determines direction enemy facing
 	Animator anim;						//For controlling animation
 	GameObject target;					//Enemy's target
+	bool TargetInSight = false;					//Determines if target is in sight
 
 	// Use this for initialization
 	void Awake () {
@@ -17,31 +18,41 @@ public class TemplateEnemyAI : MonoBehaviour {
 		anim = GetComponent<Animator>();
 	}
 	
+	// Activates when target enters trigger collider
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player")
+			TargetInSight = true;
+	}
+
+	//Determines what target leaves field of view
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Player"){
+			TargetInSight = false;
+			anim.SetFloat("Speed", 0);
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		ApproachTarget();
+		if(TargetInSight){
+			ApproachTarget();
+		}
 	}
 
 	//Moves enemy closer to target
 	void ApproachTarget(){
 		float distance = Vector2.Distance(transform.position,target.transform.position);
 
-		if(distance > detectRadius){
-			anim.SetFloat("Speed", 0);
-		}
-		else
-		{
-			FaceTarget();
-			if(distance > closeEnough){
-				transform.position = Vector2.MoveTowards(transform.position, 
-				                                         target.transform.position,
-				                                         moveSpeed * Time.deltaTime); 
-				anim.SetFloat ("Speed", 1);		//Tells animator enemy is moving
-				anim.SetBool("IsAttacking", false);
-				
-			}
+		FaceTarget();
+	
+		if(distance > closeEnough){
+			transform.position = Vector2.MoveTowards(transform.position,
+			                                         target.transform.position,
+			                                         moveSpeed * Time.deltaTime); 
+			anim.SetFloat ("Speed", 1);		//Tells animator enemy is moving	
 		}
 		Attack(distance);
+		
 	}
 
 	//Faces enemy towards target
