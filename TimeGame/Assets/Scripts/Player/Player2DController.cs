43231107a774ea2082fb99065a479246905abd3d
Christopher Luong <bumblefo@gmail.com>
2014-11-10 22:20:21 -0800

    added a little to animController and Player2DController still WIP

--- TimeGame/Assets/Scripts/Player/Player2DController.cs
+++ TimeGame/Assets/Scripts/Player/Player2DController.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class Player2DController : MonoBehaviour {
 
+	//Movement/Animations
 	public float maxSpeed = 5f;			//Sets momvement speed
 	bool facingRight = true;			//Determines direction character facing
 	Animator anim;						//Animation object
-	
+	public int health = 1;
+
+	//Hacking
+	public BoxCollider2D sword;
+	bool isHacking = false;
+
+
+	//Keys
+	public KeyCode hackKey;
+
+
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
@@ -22,6 +34,9 @@ public class Player2DController : MonoBehaviour {
 		//*Speed defined in Unity Animator Controller
 		anim.SetFloat ("Speed", Mathf.Abs (move_x));
 
+		//Changes value of Health
+		//anim.SetFloat ("Health", health); 
+
 		//Makes character move based on inputs
 		rigidbody2D.velocity = new Vector2 (move_x * maxSpeed, move_y * maxSpeed);
 
@@ -35,13 +50,35 @@ public class Player2DController : MonoBehaviour {
 		else if((move_x < 0) && facingRight){
 			Flip();
 		}
+		if (Input.GetKey(hackKey))
+		{
+
+		}
 	}
 
-	//Flips character sprite to face direction of movement
+	//Flips character sprite to face direction of movement also flips attached colliders
 	void Flip() {
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+
+	//Hack function
+	//shouldn't work well since
+	void OnTriggerEnter2D(Collider2D other) {
+		if (isHacking == true) {
+			transform.position = other.transform.position;
+			//if sword's collider is within enemy's collider
+		}
+		else if (isHacking==false) {
+		
+		}
+		else {
+			
+			
+		}
+	}
+
+
 }
