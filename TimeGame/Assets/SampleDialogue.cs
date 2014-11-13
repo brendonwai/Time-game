bb9733e8904f87a30d56f099ef19e4ec8403614a
achongco <achongco@uci.edu>
2014-11-13 15:04:08 -0800

    Added a dialogue pop up sample. Walk next to the statue thing in the upper left dialogue.

---
+++ TimeGame/Assets/SampleDialogue.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleDialogue : MonoBehaviour {
+
+	public GameObject dialogue;
+	bool display = false;
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("TESFSDFSd");
+		if(other.tag == "Player"){
+			display = true;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.tag == "Player"){
+			display = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Player")
+			display = false;
+	}
+
+	void Update(){
+		if(display)
+			dialogue.renderer.enabled = true;
+		else
+			dialogue.renderer.enabled = false;
+	}
+}
