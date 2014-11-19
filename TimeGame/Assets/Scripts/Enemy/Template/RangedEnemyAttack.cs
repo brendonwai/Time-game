01b93411c367dcd8a2c6d43469454f0895e4ac5b
Brendon Wai <brendonwai@BrendonimatoMBP.oc.cox.net>
2014-11-19 12:39:47 -0800

    testing for bullets

--- TimeGame/Assets/Scripts/Enemy/Template/RangedEnemyAttack.cs
+++ TimeGame/Assets/Scripts/Enemy/Template/RangedEnemyAttack.cs
@@ -1,35 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TemplateEnemyAttack : MonoBehaviour {
+public class RangedEnemyAttack : MonoBehaviour {
 
 	Animator anim;
 	bool TargetInRange = false;
-
+	public GameObject bullet;
+	int bulletspeed=6;
+	public GameObject player;
+	
 	void Awake(){
 		anim = GetComponentInParent<Animator>();
 	}
-
+	
 	void OnTriggerStay2D(Collider2D other){
 		if(other.tag == "Player")
 			TargetInRange = true;
 	}
-
+	
 	void OnTriggerExit2D(Collider2D other){
 		if(other.tag == "Player")
 			TargetInRange = false;
-			anim.SetBool("IsAttacking", false);
+		anim.SetBool("IsAttacking", false);
 	}
-	 
+	
 	void FixedUpdate(){
 		if(TargetInRange)
 			Attack ();
 	}
-
+	
 	void Attack(){
 		anim.SetBool("IsAttacking", true);
-		//INSERT ATTACKING CODE
-		Debug.Log (gameObject.name);
+		Instantiate (bullet, transform.position, transform.rotation);
+		Vector2 v = bullet.rigidbody2D.velocity;
+		v.y = bulletspeed;
+		bullet.rigidbody2D.velocity = v;
+		bullet.renderer.material.color = Color.blue;
 	}
-	
+
+	void GenerateBullets(){
+
+
+	}
 }
---
+++ TimeGame/Assets/Scripts/Enemy/Template/bullets.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullets : MonoBehaviour {
+	
+	public int speed = 6;
+	public int damage = 10;
+
+	// deal damage to collided target depending on color of bullet
+	// enemy shoot blue bullets --> damage player
+	// player hacked robots shoot red bullets --> damage enemies
+	// destroy 
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag=="Environment"){
+			Destroy(gameObject);
+		}
+		if (other.tag=="Background"){
+			Destroy (gameObject);
+		}
+		if (other.tag=="Player"){
+			if (renderer.material.color==Color.blue){
+				other.SendMessage("takeDamage",damage);
+				Destroy (gameObject);
+			}
+		}
+		if (other.tag=="Enemy"){
+			if (renderer.material.color==Color.red){
+				other.SendMessage("takeDamage",damage);
+				Destroy (gameObject);
+			}
+		}
+
+	}
+
+
+	void OnBecameInvisible(){
+			Destroy (gameObject);
+	}
+}
