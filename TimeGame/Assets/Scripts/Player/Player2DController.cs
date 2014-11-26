66de524a0b649ba301319725fa9e7866faf57121
Brendon Wai <brendonwai@BrendonimatoMBP.oc.cox.net>
2014-11-25 16:30:18 -0800

    Added animation when player health reaches 0 before game over scene starts

--- TimeGame/Assets/Scripts/Player/Player2DController.cs
+++ TimeGame/Assets/Scripts/Player/Player2DController.cs
@@ -6,6 +6,7 @@ public class Player2DController : MonoBehaviour {
 	public float maxSpeed = 5f;			//Sets momvement speed
 	bool facingLeft = true;			//Determines direction character facing
 	Animator anim;						//Animation object
+	bool death=false;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,28 +14,30 @@ public class Player2DController : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
+		if (!death){
 
-		//Directional command handlers
-		float move_x = Input.GetAxis ("Horizontal");    //Get input for x-axis
-		float move_y = Input.GetAxis ("Vertical");      //Get input for y-axis
-		
-		//Changes value of Speed
-		//*Speed defined in Unity Animator Controller
-		anim.SetFloat ("Speed", Mathf.Abs (move_x));
-		anim.SetFloat ("YSpeed", Mathf.Abs (move_y));
+			//Directional command handlers
+			float move_x = Input.GetAxis ("Horizontal");    //Get input for x-axis
+			float move_y = Input.GetAxis ("Vertical");      //Get input for y-axis
+			
+			//Changes value of Speed
+			//*Speed defined in Unity Animator Controller
+			anim.SetFloat ("Speed", Mathf.Abs (move_x));
+			anim.SetFloat ("YSpeed", Mathf.Abs (move_y));
 
-		//Makes character move based on inputs
-		rigidbody2D.velocity = new Vector2 (move_x * maxSpeed, move_y * maxSpeed);
+			//Makes character move based on inputs
+			rigidbody2D.velocity = new Vector2 (move_x * maxSpeed, move_y * maxSpeed);
 
-		/*
-		 * If moving in the positive x direction (right) and the character is not
-		 * facing right, makes the character sprite flip to face the right.
-		 */	
-		if((move_x < 0) && !facingLeft){
-			Flip();
-		}
-		else if((move_x > 0) && facingLeft){
-			Flip();
+			/*
+			 * If moving in the positive x direction (right) and the character is not
+			 * facing right, makes the character sprite flip to face the right.
+			 */	
+			if((move_x < 0) && !facingLeft){
+				Flip();
+			}
+			else if((move_x > 0) && facingLeft){
+				Flip();
+			}
 		}
 	}
 
@@ -48,14 +51,16 @@ public class Player2DController : MonoBehaviour {
 		GetComponent<PlayerInfo>().Health -= damage;
 		if (GetComponent<PlayerInfo>().Health<=0){
 			GetComponent<PlayerInfo>().Health=0;
-			anim.SetBool("IsDead",true);
-			PlayerDeath();
-
+			StartCoroutine(PlayerDeath());
 		}
 
 	}
 
-	void PlayerDeath(){
+	IEnumerator PlayerDeath(){
+		death = true;
+		rigidbody2D.velocity = new Vector2 (0,0);
+		anim.SetBool("IsDead",true);
+		yield return new WaitForSeconds(2.083f);
 		Application.LoadLevel ("GameOverScene");
 	}
 
