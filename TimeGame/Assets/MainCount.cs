35ed8bd1afe0cd5e4fadab9767fbb742604aae49
achongco <achongco@uci.edu>
2015-02-02 20:08:48 -0800

    Made pause menu same as gameover
    The buttons weren't responding as well for some reason.

---
+++ TimeGame/Assets/MainCount.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCount : MonoBehaviour {
+
+	public int death_count = -1;
+	public int pause_count = -1;
+
+	void Awake(){
+		DontDestroyOnLoad(this);
+	}
+
+	public void SetPause(int value){
+		pause_count = value;
+	}
+}
--- TimeGame/Assets/Scripts/Menus/GameOverManager.cs
+++ TimeGame/Assets/Scripts/Menus/GameOverManager.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 using System.Collections;
 
 public class GameOverManager : MonoBehaviour {
 
 	GameObject PlayerStatus;
 	GameObject UI_GameOver;
+
+	public Text MainComment;
+	public Text comment;
+	bool paused = false;
+
+	//Comment Stuff
+	int i = -1;						//Keeps track of what comment to display
+	int num_comments = 7;			//How many comments to cycle through
+	int d = -1;
+	int death_count = 7;
 
 	void Awake(){
 		PlayerStatus = GameObject.FindGameObjectWithTag("Player");
@@ -16,11 +27,69 @@ public class GameOverManager : MonoBehaviour {
 	void Update(){
 		if(PlayerStatus.GetComponent<Player2DController>().GameOver)
 			RunGameOver ();
+		else
+			RunPause();
+	}
+
+	void RunPause(){
+		MainComment.text = "Paused";
+		if(Input.GetKeyDown(KeyCode.P)&&!paused){
+			PlayerStatus.GetComponent<Player2DController>().paused = true;
+			
+			if(i<num_comments)
+				i++;
+			else
+				i=0;
+			comment.text = PauseComment(i);
+			
+			paused = true;
+			UI_GameOver.SetActive(true);
+			Time.timeScale = 0.0f;
+		}
+		else if(Input.GetKeyDown(KeyCode.P)&&paused)
+			ResumeGame();
+
 	}
 
 	void RunGameOver(){
+		MainComment.text = "Game Over";
+		comment.text = GameOverComment(d);
 		Time.timeScale = 0.0f;	//Pauses the game
 		UI_GameOver.active = true;		//Makes Gameover UI visible 
 
+	}
+
+	public void ResumeGame(){
+		paused = false;
+		PlayerStatus.GetComponent<Player2DController>().paused = false;
+		UI_GameOver.SetActive(false);
+		Time.timeScale = 1.0f;
+	}
+
+	string GameOverComment(int selection){
+		switch(selection){
+		case 1: return "Third times a charm.";
+		case 2: return "I lied. Fourth time. Tofu Man is rooting for you.";
+		case 3: return "You can do it. Tofu Man believes in you.";
+		case 4: return "...";
+		case 5: return "You're making Tofu Man cry.";
+		case 6: return "Tofu Man expected better from you";
+		case 7: return "";
+		default: return "";
+		}
+	}
+	
+	//Sets what comment to put during pause screen
+	string PauseComment(int selection){
+		switch(selection){
+		case 1: return "Tofu Man used to be in the game. But now he's gone.";
+		case 2: return "Everything comes back to shrooms.";
+		case 3: return "I really have nothing better to do.";
+		case 4: return "I'm being serious right now.";
+		case 5: return "Nothing.";
+		case 6: return "...";
+		case 7: return "(ﾉ◕ヮ◕)ﾉ*:･✧";
+		default: return "";
+		}
 	}
 }
