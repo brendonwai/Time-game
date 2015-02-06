a8aa1c000ded21bf67418e12bc47ce328068ac2c
achongco <achongco@uci.edu>
2015-02-06 13:34:20 -0800

    Randomize Color of Floor
    We can change color of just the walls or just the floor now. Color is
    set to randomize for the Test Floor.

---
+++ TimeGame/Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorRandomizer : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("ColorChange",1.0f, 4.0f);
+	}
+
+	void ColorChange(){
+		float red = Random.Range(0f,1.0f);
+		float blue = Random.Range(0f,1.0f);
+		float green = Random.Range(0f,1.0f);
+
+		renderer.material.color = new Color(red,blue,green, 1.0f);
+	}
+}
