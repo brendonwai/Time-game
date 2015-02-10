7bf68d0f2fa49ea74613377d54cc018b339c8eca
brendonwai <bwai@uci.edu>
2015-02-10 12:49:31 +0800

    resetting git

--- TimeGame/Assets/Scripts/Enemy/GlobalEnemyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GlobalEnemyInfo : MonoBehaviour {
-	//Information that must be accessed by all enemies
-
-	public int CanSeePlayer = 0;	//Records how many enemies can see the player at the moment
-}
