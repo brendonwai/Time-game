f27900fadcfac0885dd048e0ac8a2135b48bc51f
Christopher Luong <bumblefo@gmail.com>
2015-02-09 18:53:53 -0800

    Starting on Flipping the scene for level variation

---
+++ TimeGame/Assets/Scripts/Menus/FlipSceneVariation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipSceneVariation : MonoBehaviour {
+
+	//Don't enable this unless you're testing.
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Level");
+		foreach(GameObject obj in allObjects) {
+			obj.transform.localScale = new Vector3 (obj.transform.localScale.x * -1, obj.transform.localScale.y, obj.transform.localScale.z);
+		}
+	}
+}
