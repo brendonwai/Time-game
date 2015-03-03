41298c78a1eac083fd690b7d3e468c896fc4013d
Ian Tokuyama <igolftokuyama@gmail.com>
2015-03-03 10:28:36 -0800

    pulling

---
+++ TimeGame/Assets/Scripts/PickUpChest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PickUpChest : MonoBehaviour {
+ 
+void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			Destroy (gameObject);
+            Debug.Log ("why wont you work ;_;");
+		}
+	}
+}
