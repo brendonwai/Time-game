f8f718ecf3294cac9dfc2dbff713e23f25448eb4
Christopher Luong <bumblefo@gmail.com>
2015-03-04 16:43:29 -0800

    Fixed small null reference exception caused by health drone not having a hp bar

--- TimeGame/Assets/Scripts/Enemy/BasicSMessage.cs
+++ TimeGame/Assets/Scripts/Enemy/BasicSMessage.cs
@@ -8,7 +8,9 @@ public class BasicSMessage : MonoBehaviour {
 		if(GetComponentInParent<EnemyInfo>().Health>=0){
 			//reduce health by amount of damage
 			GetComponentInParent<EnemyInfo>().Health -= damage;
-			GetComponentInParent<EnemyInfo> ().healthBar.value = GetComponentInParent<EnemyInfo> ().Health;
+			if (GetComponentInParent<EnemyInfo> ().enemyType != 2) {	//Health Drone doesn't have a healthbar
+				GetComponentInParent<EnemyInfo> ().healthBar.value = GetComponentInParent<EnemyInfo> ().Health;
+			}
 			//sprite flashes red upon taking damage
 			GetComponentInParent<Renderer>().material.color = Color.red;
 			yield return new WaitForSeconds (.1f);
